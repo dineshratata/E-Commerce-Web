e4a992c8b70ddc4fe7a6a4015761975957e0ccf0
dineshratata <dineshstallone15196@gmail.com>
2024-03-19 23:48:53 +0530

    CategorgyController Created

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,139 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+
+        public ActionResult<Category> Get()
+        {
+                 var _categories   = _dbContext.Category.ToList();
+
+                 return Ok(_categories);
+
+
+
+
+
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpPost]
+        public ActionResult Create([FromBody] Category category )
+        {
+            _dbContext.Category.Add(category);
+            _dbContext.SaveChanges();
+            return Ok(category);
+
+
+        }
+
+
+
+        [HttpGet]
+        [Route("Details")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<Category>GetByid(int id)
+        {
+
+                  var categoryDetails  =  _dbContext.Category.FirstOrDefault(x => x.Id == id);
+
+
+               if(categoryDetails == null)
+            {
+                return NotFound($"You have Entered Wrong {id} ");
+
+            }
+                   
+                  return Ok(categoryDetails);
+                   
+
+
+        }
+
+
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult Delete(int id)
+        {
+
+                    var dataForRemoval  =  _dbContext.Category.FirstOrDefault(x => x.Id == id);
+
+                     _dbContext.Category.Remove(dataForRemoval);
+
+            return Ok("Record Deleted successFully");
+
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult Update(int id)
+
+
+
+        {
+
+            if(id == 0)
+            {
+
+                BadRequest("Enter a Valid Number");
+            }
+
+
+            var dataForRemoval = _dbContext.Category.FirstOrDefault(x => x.Id == id);
+
+            _dbContext.Category.Update(dataForRemoval);
+
+            return NoContent();
+
+
+        }
+
+
+
+
+
+
+
+
+        //[HttpDelete]
+        //public ActionResult Delete(int id)
+        //{
+        //    if(id == 0)
+        //    {
+        //        BadRequest("Enter a valid Number");
+
+        //    }
+
+        //              _dbContext.
+
+
+
+
+
+        //}
+
+
+    }
+}
--- E-Commerce/Models/Category.cs
+++ E-Commerce/Models/Category.cs
@@ -1,11 +1,15 @@
-﻿namespace E_Commerce.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models
 {
     public class Category
     {
 
-
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
 
