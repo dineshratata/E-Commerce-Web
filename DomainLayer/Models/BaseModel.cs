5ce1c166be56083b2c0c422b3b211e64e3897406
dineshratata <dineshstallone15196@gmail.com>
2024-03-20 08:30:38 +0530

    BaseModelCreated

--- DomainLayer/Models/BaseModel.cs
+++ DomainLayer/Models/BaseModel.cs
@@ -1,23 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DomainLayer.Models
 {
-    public class Category:BaseModel
+    public  class BaseModel
     {
+        public int Id { get; set; }
 
-     
-
-        [Required]
-        public string Name { get; set; }
-
-
+        public DateTime CreateOn { get; set; } = DateTime.UtcNow;
 
 
     }
-
 }
