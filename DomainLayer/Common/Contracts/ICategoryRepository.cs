b0678b9e1c09aa30bffac87a2668be38d06a4df8
dineshratata <dineshstallone15196@gmail.com>
2024-03-20 10:50:29 +0530

    CategoryUpdateRepositoryCreated

---
+++ DomainLayer/Common/Contracts/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Common.Contracts
+{
+    public interface ICategoryRepository : IGenericRepository<Category> 
+    {
+
+        Task UpdateAsync (Category category);
+
+
+    }
+}
---
+++ InfrastructureLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using DomainLayer.Common.Contracts;
+using DomainLayer.Models;
+using InfrastuctureLayer.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Repositories
+{
+    public class CategoryRepository : GenericeRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext applicationDbContext ) :base(applicationDbContext)
+        {
+            
+
+        }
+
+
+        public async Task UpdateAsync(Category category)
+        {
+            _dbContext.Update(category);
+             await _dbContext.SaveChangesAsync();
+
+
+
+        }
+    }
+}
+
